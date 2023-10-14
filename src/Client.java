a70efc9353a8062d0cfb45844dc32975a058605d
truix12 <truix12@gmail.comgit config --global user.email truix12@gmail.comgit config --global user.email truix12@gmail.com>
2023-10-14 15:39:45 +0200

    create Client.java class

---
+++ src/Client.java
@@ -0,0 +1,11 @@
+public class Client {
+    private int clientId;
+    private String clientName;
+    private String clientSurname;
+
+    public Client(int clientId, String clientName, String clientSurname) {
+        this.clientId = clientId;
+        this.clientName = clientName;
+        this.clientSurname = clientSurname;
+    }
+}
